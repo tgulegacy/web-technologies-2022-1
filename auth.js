752f0c0d3804c1665c4b999c08994f08f5b1882a
anyaphmv <stud0000231077@study.utmn.ru>
2022-04-20 13:45:03 +0500

    6 tasks

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 import Form from "./components/form.js";
 import {Auth} from "./services/auth.js";
-import {Todos} from "./services/todos.js"
+import {Todos} from "./services/todos.js";
 
 if (document.readyState === 'loading') {
     document.addEventListener('DOMContentLoaded', init)
--- services/auth.js
+++ services/auth.js
@@ -28,7 +28,6 @@ export class Auth {
             this.setUserInfo({}, true)
         }
 
-
         return data
     }
 
