7f2e7142f12f7d5932dd28d43a0cb61ac76a72f1
Pavel <p.potemkin.work@gmail.com>
2022-05-19 12:55:35 +0500

    lesson-11

---
+++ model/products.php
@@ -0,0 +1,15 @@
+<?php
+	include_once('model/db.php');
+
+	function productsAll() : array{
+		$sql = "SELECT * FROM products";
+		$query = dbQuery($sql);
+		return $query->fetchAll();
+	}
+    
+    function productById(string $id) {
+        $sql = "SELECT * FROM products WHERE id=:id";
+        $query = dbQuery($sql, ['id' => $id]);
+        return $query->fetchAll();
+    }
+    
